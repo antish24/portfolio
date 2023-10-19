5512e3703aa601486cfba61d1404dd6de67e5af5
antish <101444378+antish24@users.noreply.github.com>
2023-10-19 11:05:01 +0300

    Fetch work

--- src/app/OurWork/page.js
+++ src/app/OurWork/page.js
@@ -10,17 +10,7 @@ export const metadata = {
   description: "Developer and Graphics Designer",
 };
 
-async function getProjects() {
-  const res = await fetch(`${Url}/api/projects`, {
-    cache: "force-cache",
-  });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
 
 const OurWork =async () => {
   const projects=[]
