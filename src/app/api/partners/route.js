b6f35750b615e14cc4f7f68885147a9f19951043
antish24 <aantenehh@gmail.com>
2023-10-23 09:22:31 +0300

    fetch updated

--- src/app/api/partners/route.js
+++ src/app/api/partners/route.js
@@ -1,14 +1,13 @@
 import { NextResponse } from "next/server";
 import connect from "@/backend/config/db";
 import Partners from "@/backend/models/Partners";
-import Project from "@/backend/models/Project";
 
 export const GET = async (request) => {
 
   try {
     await connect();
 
-    const partners = await Project.find();
+    const partners = await Partners.find();
 
     return new NextResponse(JSON.stringify(partners), { status: 200 });
   } catch (err) {
