6916be4abc4fd1f3a85e0d535dd875dfa8b900ab
antish24 <aantenehh@gmail.com>
2023-10-23 10:28:08 +0300

    fetch updated

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import connect from "@/backend/config/db";
 import Contact from "@/backend/models/Contact";
+import nodemailer from 'nodemailer'
 
 export const POST = async (request) => {
   const { nameValue,emailValue ,msgValue} = await request.json()
@@ -15,6 +16,35 @@ export const POST = async (request) => {
     });
 
       await newContact.save();
+
+
+      //send email
+
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: '24ishwork@gmail.com', // Replace with your Gmail address
+          pass: '2023@#ish', // Replace with your Gmail password or app-specific password
+        },
+      });
+  
+      const mailOptions = {
+        from: '24ishwork@gmail.com', // Replace with your Gmail address
+        to: '24ishwork@gmail.com', // Replace with your personal email address
+        subject: 'New Contact Form Submission',
+        text: `Name: ${nameValue} \n Email: ${emailValue}\nMessage: ${msgValue}`,
+      };
+  
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error('Error sending email:', error);
+        } else {
+          console.log('Email sent:', info.messageId);
+        }
+      });
+
+
+
       return new NextResponse("Msg sent", {
         status: 201,
       });
